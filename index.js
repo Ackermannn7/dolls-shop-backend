3031b1d1be7349ff2bb9f3a774ffdd36ae7e1dd3
Aleksey Rudyk <95295778+Ackermannn7@users.noreply.github.com>
2023-04-14 15:30:35 +0300

    login, register, getInfo about user

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import { registerValidation } from "./validations/auth.js";
-import { validationResult } from "express-validator";
 
-import UserModel from "./models/User.js";
+import checkAuth from "./utils/checkAuth.js";
+import * as UserController from "./controllers/UserController.js";
 
 mongoose
   .connect(
@@ -18,29 +16,11 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("111 Hello World!");
-});
+app.post("/auth/login", UserController.login);
 
-app.post("/auth/register", registerValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
-  }
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
+app.post("/auth/register", registerValidation, UserController.register);
 
-  const doc = new UserModel({
-    fullName: req.body.fullName,
-    email: req.body.email,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash,
-  });
-
-  const user = await doc.save();
-  res.json(user);
-});
+app.get("/auth/me", checkAuth, UserController.getMe);
 
 app.listen(4444, (err) => {
   if (err) console.log(err);
