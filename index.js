c08470b6bff2aacfa596884f2fe05c681d406e30
Ackermannn7 <aleksey.rudyksm@gmail.com>
2023-05-12 14:00:20 +0300

    dolls photos to database

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/photos", express.static("photos"));
 
 app.get("/dolls", DollsController.getAllDolls);
 app.post("/dolls", checkAuth, DollsController.createDoll);
